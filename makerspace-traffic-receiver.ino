9b58d7d1fbc077651295b4917e25e6664dec85cf
Chanan Loh <hotdroid3@gmail.com>
2018-10-07 19:05:26 +0800

    Communication with Raspberry Pi Zero

--- makerspace-traffic-receiver.ino
+++ makerspace-traffic-receiver.ino
@@ -1,19 +1,19 @@
-#include "SIGFOX.h"                         //  Include the unabiz-arduino library.
+//#include "SIGFOX.h"                         //  Include the unabiz-arduino library.
 
 #define INPUT_PIN 12
 
-static const Country country = COUNTRY_SG;  //  Set this to your country to configure the Sigfox transmission frequencies.
-static const bool useEmulator = false;      //  Set to true if using UnaBiz Emulator.
-static const String device = "2C3068";       //  Set this to your device name if you're using UnaBiz Emulator.
-static const bool echo = true;              //  Set to true if the Sigfox library should display the executed commands.
+//static const Country country = COUNTRY_SG;  //  Set this to your country to configure the Sigfox transmission frequencies.
+//static const bool useEmulator = false;      //  Set to true if using UnaBiz Emulator.
+//static const String device = "2C3068";       //  Set this to your device name if you're using UnaBiz Emulator.
+//static const bool echo = true;              //  Set to true if the Sigfox library should display the executed commands.
 //static String response;
 
-static UnaShieldV2S transceiver(country, useEmulator, device, echo);  //Create transceiver object
+//static UnaShieldV2S transceiver(country, useEmulator, device, echo);  //Create transceiver object
 
-const unsigned int transmit_message_interval = 10;                      //Sets the duration of the interval (in minutes) between successive message transmissions.
+const unsigned int transmit_message_interval = 5;                      //Sets the duration of the interval (in minutes) between successive message transmissions.
 unsigned long previous_timestamp_m = 0L;                                //Timestamp of the beginning of the previous iteration of void loop() in milliseconds.
 unsigned long previous_iteration_duration = 0L;                          //Duration of execution of previous iteration of void loop() in milliseconds.
-long countdown = transmit_message_interval * 60L * 1000L;                //Countdown timer for message transmission.
+long countdown = transmit_message_interval * 1000L;                //Countdown timer for message transmission. **REMEMBER TO CHANGE BACK TO MINUTES
 
 unsigned int counter = 0;
 unsigned long duration_low = 0L;
@@ -24,9 +24,13 @@ void setup()
 {
   pinMode(INPUT_PIN, INPUT);
   Serial.begin(9600);
-  if(!transceiver.begin())
+//  if(!transceiver.begin())
+//  {
+//    stop(F("Unable to init Sigfox module, something is missing"));
+//  }
+  while(!Serial)
   {
-    stop(F("Unable to init Sigfox module, something is missing"));
+    ; // wait for serial port to connect. Needed for native USB
   }
 //  transceiver.sendMessageAndGetResponse("0102030405060708090a0b0c", response);
 //  Serial.print(F("gvyvgjkbkhjkgvjghvy is "));
@@ -74,6 +78,15 @@ unsigned int count_when_blocking()
   return count;
 }
 
+void writeCounterToSerial(unsigned int message)
+{
+  if(Serial.availableForWrite() > 7)
+  {
+    Serial.println(message);
+    Serial.flush(); //Waits for the transmission of outgoing serial data to complete.
+  }
+}
+
 void loop()
 {
   unsigned long current_timestamp_m = millis();
@@ -92,19 +105,24 @@ void loop()
   }
 
   if(countdown <= 0){
-    Serial.print("Sending counter = ");
-    Serial.println(counter);
-    transceiver.sendString(String(counter));
+//    Serial.print("Sending counter = ");
+//    Serial.println(counter);
+//    transceiver.sendString(String(counter));
+    writeCounterToSerial(counter);
 
 
     //Reset after sending counter.
     counter = 0;
-    countdown = transmit_message_interval * 60L * 1000L;
+    countdown = transmit_message_interval * 1000L; //REMEMBER TO RESET BACK TO MINUTES
     duration_low = 0L;
     blocking_flag =  false;
   }
   
-  Serial.println(counter);
+//  Serial.println(counter);
 }
+
+
+
+
 
 
